e11dfcc00f6163dc9ffc385379b531152f08a18b
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-10-17 13:40:59 +0100

    added vpc

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_s3_bucket" "khoovi_bucket" {
-  bucket = "tf-khoovi-bucket"
+resource "aws_s3_bucket" "prod_tf_khoovi_course" {
+  bucket = "tf-course-khoovi-bucket"
   acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {}
